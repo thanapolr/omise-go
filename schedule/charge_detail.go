66cbe0c5e901e4f2c6a3c373e85a2e5fa41fb30c
Thanapol Rojanapanpat <>
2021-04-29 18:52:43 +0700

    change datatype to int64

--- schedule/charge_detail.go
+++ schedule/charge_detail.go
@@ -2,7 +2,7 @@ package schedule
 
 // ChargeDetail represents charge detail for schedule object.
 type ChargeDetail struct {
-	Amount      int                    `json:"amount"`
+	Amount      int64                  `json:"amount"`
 	Currency    string                 `json:"currency"`
 	Customer    string                 `json:"customer"`
 	Card        *string                `json:"card"`
